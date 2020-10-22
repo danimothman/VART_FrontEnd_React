115fdcba96b2cc03afc9370d3475eab46e27bf54
Hwankook Kim <hwankook.kim9@gmail.com>
2020-10-22 11:13:20 +0900

    companyData2.js 가짜 데이터 추가

---
+++ src/data/companyData2.js
@@ -0,0 +1,86 @@
+export const initialState = {
+
+    users: [
+        {
+            id: 1,
+            username: "KIM",
+            email: "1@naver.com",
+            // tokenLogo: "tokenLogo",
+            tokenName: "tokenName",
+            projectType: "projectType",
+            companyName: "companyName",
+            companyEstablishment: "companyEstablishment",
+            companyLocation: "companyLocation",
+            stateJurisdiction: "stateJurisdiction",
+            memberName: "memberName",
+            memberEducation: "memberEducation",
+            memberExperience: "memberExperience",
+            developerLeaderName: "developerLeaderName",
+            developerLeaderEducation: "developerLeaderEducation",
+            developerLeaderExperience: "developerLeaderExperience",
+            // mkName: "mkName",
+            // mkLocation: "mkLocation",
+            // mkCpdate: "mkCpdate",
+            // mkFounder: "mkFounder",
+            // mileStone: "mileStone",
+            // moveNum: "moveNum",
+            // myWallet: "myWallet",
+            active: true
+        }
+    ],
+
+    inputs: {
+        username: '',
+        email: '',
+        // tokenLogo: '',
+        tokenName: '',
+        projectType: '',
+        companyName: '',
+        companyEstablishment: '',
+        companyLocation: '',
+        stateJurisdiction: '',
+        memberName: '',
+        memberEducation: '',
+        memberExperience: '',
+        developerLeaderName: '',
+        developerLeaderEducation: '',
+        developerLeaderExperience: '',
+        // mkName: '',
+        // mkLocation: '',
+        // mkCpdate: '',
+        // mkFounder: '',
+        // mileStone: '',
+        // moveNum: '',
+        // myWallet: ''
+    }
+}
+
+export const companyData2 = [
+    {
+        id: 1,
+        username: "KIM",
+        email: "1@naver.com",
+        // tokenLogo: "tokenLogo",
+        tokenName: "tokenName",
+        projectType: "projectType",
+        companyName: "companyName",
+        companyEstablishment: "companyEstablishment",
+        companyLocation: "companyLocation",
+        stateJurisdiction: "stateJurisdiction",
+        memberName: "memberName",
+        memberEducation: "memberEducation",
+        memberExperience: "memberExperience",
+        developerLeaderName: "developerLeaderName",
+        developerLeaderEducation: "developerLeaderEducation",
+        developerLeaderExperience: "developerLeaderExperience",
+        // mkName: "mkName",
+        // mkLocation: "mkLocation",
+        // mkCpdate: "mkCpdate",
+        // mkFounder: "mkFounder",
+        // mileStone: "mileStone",
+        // moveNum: "moveNum",
+        // myWallet: "myWallet",
+        active: true
+    }
+]
+
