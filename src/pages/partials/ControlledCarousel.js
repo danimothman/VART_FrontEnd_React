c318508f0a9eb365c8074e6d245365b4474ae5c8
Hwankook Kim <hwankook.kim9@gmail.com>
2020-10-29 12:18:24 +0900

    베너 이미지 추가

--- src/pages/partials/ControlledCarousel.js
+++ src/pages/partials/ControlledCarousel.js
@@ -5,16 +5,17 @@ import '../../images/Dawn1.jpg';
 // import logo from './logo.svg';
 // import TutorLogin from "./components/TutorLogin";
 
-import { Button, Carousel } from 'react-bootstrap';
+import { Button, Carousel, Image } from 'react-bootstrap';
 
 const ControlledCarouselBanel = ({ history }) => {
     return (
         <Carousel>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={'../../images/Dawn1.jpg'}
+                <Image
+                    className=""
+                    src={require("../../images/Dawn1.jpg")}
                     alt="First slide"
+                    fluid
                 />
                 <Carousel.Caption>
                     <h3>First slide label</h3>
@@ -23,11 +24,11 @@ const ControlledCarouselBanel = ({ history }) => {
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
-                    src="../../images/Dawn1.jpg/800x400?text=First slide&bg=373940"
-                    alt="Third slide"
+                    className=""
+                    src={require("../../images/Dawn2.gif")}
+                    alt="Second slide"
+                    fluid
                 />
-
                 <Carousel.Caption>
                     <h3>Second slide label</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -35,11 +36,11 @@ const ControlledCarouselBanel = ({ history }) => {
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                    className="d-block w-100"
-                    src="../../images/Dawn1.jpg/800x400?text=First slide&bg=373940"
+                    className=""
+                    src={require("../../images/Dawn3.jpg")}
                     alt="Third slide"
+                    fluid
                 />
-
                 <Carousel.Caption>
                     <h3>Third slide label</h3>
                     <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
